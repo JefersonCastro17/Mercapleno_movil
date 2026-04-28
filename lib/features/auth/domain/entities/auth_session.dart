3e8659015cf24d0431737663c382b0593d3add27
JefersonCastro17 <jefersonjairbernalcastro172129@gmail.com>
2026-04-27 22:28:41 -0500

    chore: limpiar main y dejar rama base

--- lib/features/auth/domain/entities/auth_session.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mercapleno_appv1/features/auth/domain/entities/auth_user.dart';
-
-class AuthSession {
-  const AuthSession({required this.user, required this.token});
-
-  final AuthUser user;
-  final String token;
-}
